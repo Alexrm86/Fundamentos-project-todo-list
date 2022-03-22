6ac63da792005f64664cea76d86cfbdad74a4cca
Alex Resende Machado <alex.magal@gmail.com>
2022-03-22 15:50:55 -0300

    requisitos de 1 a 4

---
+++ script.js
@@ -0,0 +1,30 @@
+const criarTarefa = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+
+
+function addNovasTarefas() {
+    criarTarefa.addEventListener('click', () => {
+        const textoTarefas = document.getElementById('texto-tarefa');
+        const itemTarefa = document.createElement('li');
+        if (textoTarefas.value === '') {
+            alert('Digite uma tarefa');
+        } else {
+            itemTarefa.innerText = textoTarefas.value;
+            listaTarefas.appendChild(itemTarefa);
+            textoTarefas.value = '';
+        }
+    });
+}
+addNovasTarefas();
+
+function criarBotaoTarefas(Adicione) {
+    let tarefaBotao = document.querySelector('#lista-tarefas');
+    let novoBotao = document.createElement('criar-tarefa');
+    let novoBotaoID = 'texto-tarefa';
+
+    novoBotao.innerHTML = Adicione;
+    novoBotao.id = novoBotaoID;
+    tarefaBotao.appendChild(novoBotao);
+};
+
+criarBotaoTarefas("Adicione");
