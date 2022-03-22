e23babb8c8a8009ac5e4973e97a5814183fd401d
Alex Resende Machado <alex.magal@gmail.com>
2022-03-22 20:17:29 -0300

    requisitos de 9-14

--- script.js
+++ script.js
@@ -1,44 +1,117 @@
-const criarTarefa = document.getElementById('criar-tarefa');
-const listaTarefas = document.getElementById('lista-tarefas');
+const createTaskButton = document.querySelector('#criar-tarefa');
+const toDoList = document.querySelector('#lista-tarefas');
+
+createTaskButton.addEventListener('click', () => {
+    const taskText = document.querySelector('#texto-tarefa');
+    const newTask = document.createElement('li');
+    newTask.innerText = taskText.value;
+    newTask.className = 'task';
+    toDoList.appendChild(newTask);
+    taskText.value = '';
+});
 
 
-function addNovasTarefas() {
-    criarTarefa.addEventListener('click', () => {
-        const textoTarefas = document.getElementById('texto-tarefa');
-        const itemTarefa = document.createElement('li');
-        if (textoTarefas.value === '') {
-            alert('Digite uma tarefa');
-        } else {
-            itemTarefa.innerText = textoTarefas.value;
-            listaTarefas.appendChild(itemTarefa);
-            textoTarefas.value = '';
+toDoList.addEventListener('click', (event) => {
+    const grayLi = document.querySelector('.gray');
+    if (grayLi == null) {
+        event.target.classList.add('gray');
+    } else {
+        grayLi.classList.remove('gray');
+        event.target.classList.add('gray');
+    }
+});
+
+
+toDoList.addEventListener('dblclick', (event) => {
+    event.target.classList.toggle('completed');
+});
+
+
+const eraseAllButton = document.querySelector('#apaga-tudo');
+
+eraseAllButton.addEventListener('click', () => {
+    toDoList.innerHTML = '';
+});
+
+const removeFinishedButton = document.querySelector('#remover-finalizados');
+
+removeFinishedButton.addEventListener('click', () => {
+    const completed = document.querySelectorAll('.completed');
+    for (let i = 0; i < completed.length; i += 1) {
+        toDoList.removeChild(completed[i]);
+    }
+});
+
+
+const saveTasksButton = document.querySelector('#salvar-tarefas');
+
+saveTasksButton.addEventListener('click', () => {
+    const li = document.querySelectorAll('.task');
+    const tasks = [];
+    const classTasks = [];
+    for (let i = 0; i < li.length; i += 1) {
+        tasks.push(li[i].innerText);
+        classTasks.push(li[i].className);
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('classTasks', JSON.stringify(classTasks));
+});
+
+window.onload = () => {
+    const retrieveTasks = JSON.parse(localStorage.getItem('tasks'));
+    const retrieveClassTasks = JSON.parse(localStorage.getItem('classTasks'));
+    if (retrieveTasks !== null) {
+        for (let i = 0; i < retrieveTasks.length; i += 1) {
+            const newTask = document.createElement('li');
+            newTask.innerText = retrieveTasks[i];
+            newTask.className = retrieveClassTasks[i];
+            toDoList.appendChild(newTask);
         }
-    });
-}
-addNovasTarefas();
-
-function criarBotaoTarefas(Adicione) {
-    let tarefaBotao = document.querySelector('#lista-tarefas');
-    let novoBotao = document.createElement('btn-primary');
-    let novoBotaoID = 'criar-tarefa';
-
-    novoBotao.innerHTML = Adicione;
-    novoBotao.id = novoBotaoID;
-    tarefaBotao.appendChild(novoBotao);
+    }
 };
 
-criarBotaoTarefas("Adicione");
+const moveUpButton = document.querySelector('#mover-cima');
 
-function colorItemList() {
-    listaTarefas.addEventListener('click', (event) => {
-        const Event = event.target;
-        const Itemlist = document.getElementsByTagName('li');
-        if (Event.localName === 'li') {
-            for (let index = 0; index < Itemlist.length; index += 1) {
-                Itemlist[index].classList.remove('selected');
-            }
-            Event.classList.add('selected');
+moveUpButton.addEventListener('click', () => {
+    const grayLi = document.querySelector('.gray');
+    if (grayLi !== null) {
+        const previousTask = grayLi.previousElementSibling;
+        if (previousTask !== null) {
+            const transition1 = grayLi.innerText;
+            const transition2 = previousTask.innerText;
+            grayLi.innerText = transition2;
+            previousTask.innerText = transition1;
+            const transition3 = grayLi.className;
+            const transition4 = previousTask.className;
+            grayLi.className = transition4;
+            previousTask.className = transition3;
         }
-    });
-}
-colorItemList();
+    }
+});
+
+const moveDownButton = document.querySelector('#mover-baixo');
+
+moveDownButton.addEventListener('click', () => {
+    const grayLi = document.querySelector('.gray');
+    if (grayLi !== null) {
+        const previousTask = grayLi.nextElementSibling;
+        if (previousTask !== null) {
+            const transition1 = grayLi.innerText;
+            const transition2 = previousTask.innerText;
+            grayLi.innerText = transition2;
+            previousTask.innerText = transition1;
+            const transition3 = grayLi.className;
+            const transition4 = previousTask.className;
+            grayLi.className = transition4;
+            previousTask.className = transition3;
+        }
+    }
+});
+
+
+const removeSelected = document.querySelector('#remover-selecionado');
+
+removeSelected.addEventListener('click', () => {
+    const grayLi = document.querySelector('.gray');
+    toDoList.removeChild(grayLi);
+});
